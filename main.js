43903905f6337fbce133bc9dce682ecfa4b08db5
Lavigne Lucindo <112216899+LavigneLucindo@users.noreply.github.com>
2024-06-12 18:50:31 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ for(let i=0;i< botoes.length;i++){
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-5");
+const tempoObjetivo1 = new Date("2024-10-5");
 const tempoObjetivo2 = new Date("2024-12-5");
 const tempoObjetivo3 = new Date("2024-12-30");
 const tempoObjetivo4 = new Date("2025-02-1");
@@ -42,12 +42,11 @@ function calculaTempo(tempoObjetivo){
 }
 
 function atualizaCronometro(){
-    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];
-    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
     for (let i=0; i<contadores.length; i++){
-        //contadores[i].textContent = calculaTempo(tempos[i]);
+        document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+        document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+        document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
